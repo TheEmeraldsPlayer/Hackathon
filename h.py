69031db6948d6621d8994b5fa9de6478bc3fb209
PincerSharx <112465643+PincerSharx@users.noreply.github.com>
2022-08-30 15:13:41 +0800

    Update h.py

--- h.py
+++ h.py
@@ -2,7 +2,3 @@ import turtle
 a = turtle.Turtle()
 b = turtle.Screen()
 b.bgcolor('green')
-heh
-I cannot code
-
-basically what we first is: 1. think of a solution; 2. think from perspective of sources of climate change: sources and how we can curb; 3. start coding on whatever aspect is most important; 4. me go find slides template; 5. vin u try teach joseph we got time man 
